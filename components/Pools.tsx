5f82fbd3fdd8f7a246a629321979b61ad8c2e20a
Pochetes <robertomiguel2001@gmail.com>
2022-06-12 15:55:32 -0700

    implemented navbar w/ gammaswap products - connect wallet feature configured with ethers.js

---
+++ components/Pools.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pools = () => {
+    return (
+        <div>Pools Stuff</div>
+    )
+}
+
+export default Pools
---
+++ components/Positions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Positions = () => {
+    return (
+        <div>Positions Stuff</div>
+    )
+}
+
+export default Positions
