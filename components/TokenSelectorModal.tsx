29797e82849752941070a1811ac92450d8bedda0
Pochetes <robertomiguel2001@gmail.com>
2022-06-18 17:47:37 -0700

    finished token selector modal design

--- components/TokenSelectorModal.tsx
+++ components/TokenSelectorModal.tsx
@@ -1,10 +1,21 @@
-import React, { Dispatch, SetStateAction, ReactElement } from 'react'
-import { Dialog } from '@headlessui/react'
+import React, { Dispatch, SetStateAction, ReactElement, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XIcon } from '@heroicons/react/solid'
+import Tokens from './Tokens'
 
 const style = {
     modalWrapper: "relative z-50",
-    modalOverlay: "fixed inset-0 bg-black/40 flex items-center justify-center p-4",
-    modal: "bg-white rounded max-w-sm max-h-sm p-8",
+    modalOverlay: "fixed inset-0 bg-black/50 flex items-center justify-center p-4",
+    modal: "bg-gray-800 rounded-xl max-w-sm max-h-sm p-8 flex flex-col text-white drop-shadow-lg",
+    headingContainer: "flex",
+    closeIcon: "w-7 h-7 ml-auto cursor-pointer",
+    modalTitle: "font-semibold text-xl",
+    headingBreak: "mt-2 opacity-10",
+    tokensContainer: "mt-4 space-y-1 w-64",
+    tokenContainer: "flex text-md hover:bg-gray-700 rounded-lg p-3 cursor-pointer",
+    tokenImg: "w-7 h-7",
+    tokenSymbol: "ml-3 font-semibold",
+    tokenBalance: "ml-auto",
     modalButton: "border-black border-solid border rounded mx-2 mt-8 py-1 px-2",
 }
 
@@ -15,32 +26,47 @@ type ModalProps = {
 
 const TokenSelectorModal = ({ isOpen, setIsOpen }: ModalProps): ReactElement => {
     return (
+        <Transition appear show={isOpen} as={Fragment}>
         <Dialog
-        open={isOpen}
+        as={"div"}
         onClose={() => setIsOpen(false)}
         className={style.modalWrapper}
         >
-            <div className={style.modalOverlay}>
-                <Dialog.Panel className={style.modal}>
-                    <Dialog.Title>Select a Token</Dialog.Title>
-                    <Dialog.Description>
-                        Description of dialog contents.
-                    </Dialog.Description>
-                    <button
-                    onClick={() => setIsOpen(false)}
-                    className={style.modalButton}
-                    >
-                        Cancel
-                    </button>
-                    <button
-                    onClick={() => setIsOpen(false)}
-                    className={style.modalButton}
-                    >
-                        Confirm
-                    </button>
-                </Dialog.Panel>
-            </div>
+            <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            >
+                <div className={style.modalOverlay}>
+                    <Dialog.Panel className={style.modal}>
+                        <div className={style.headingContainer}>
+                            <Dialog.Title className={style.modalTitle}>Select a Token</Dialog.Title>
+                            <XIcon
+                            className={style.closeIcon}
+                            onClick={() => setIsOpen(false)}
+                            />
+                        </div>
+                        <hr className={style.headingBreak}></hr>
+                        <div className={style.tokensContainer}>
+                            {Tokens.map(token => {
+                                return (
+                                    <div className={style.tokenContainer}>
+                                        <img src={token.imgPath} className={style.tokenImg} />
+                                        <div className={style.tokenSymbol}>{token.symbol}</div>
+                                        <div className={style.tokenBalance}>0</div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </Dialog.Panel>
+                </div>
+            </Transition.Child>
         </Dialog>
+        </Transition>
     )
 }
 
