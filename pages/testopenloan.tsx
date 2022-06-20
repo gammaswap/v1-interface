608558baeec1cce52acffa54c00eac1d2bf4f8eb
TBomberman <TBomberman@users.noreply.github.com>
2022-06-20 13:53:41 -0700

    Added styling for open loan.

--- pages/testopenloan.tsx
+++ pages/testopenloan.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from 'next'
 import OpenLoan from '../components/OpenLoan/OpenLoan'
 import { ChakraProvider } from '@chakra-ui/react'
+import { 
+    extendTheme,
+} from '@chakra-ui/react';
+import { Button, FormLabel, Text, NumberInput } from '../components/OpenLoan/OpenLoanStyle'
+
+const theme = extendTheme({
+    components: {
+        Button,
+        FormLabel,
+        Text,
+        NumberInput
+    },
+})
 
 const Testopenloan: NextPage = () => {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <OpenLoan />
         </ChakraProvider>
     )
