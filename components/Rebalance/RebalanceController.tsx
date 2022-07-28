2be2966932839591a87ff7e7d82b98fba741aba7
UsamaSaleem14 <usamasaleem0148@gmail.com>
2022-07-28 19:18:56 +0500

    Added custom hook

--- components/Rebalance/RebalanceController.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import RebalanceView from './RebalanceView'
-
-const RebalanceController = () => {
-  return <RebalanceView />
-}
-
-export default RebalanceController
