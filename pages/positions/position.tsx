c076165028d2df6e7bec05672110230d5c1223a9
UsamaSaleem14 <usamasaleem0148@gmail.com>
2022-08-04 10:33:16 +0500

    Position table color fix

--- pages/positions/position.tsx
+++ pages/positions/position.tsx
@@ -9,11 +9,11 @@ const PositionPage: NextPage = () => {
   const style = {
     wrapper: 'w-screen flex flex-col',
     tableContainer:
-      'flex flex-col overflow-x-auto relative bg-white mx-20 p-4 rounded-lg bg-gray-50 dark:bg-gray-700 dark:text-gray-400',
+      'flex flex-col overflow-x-auto relative bg-white mx-20 p-4 rounded-lg bg-gray-50 bg-gray-700 text-gray-400',
     tableData: 'w-full text-sm text-left text-gray-500 datk:text-gray-400',
     tableHead: 'flex text-xs text-gray-700 uppercase pl-2 pr-4 border-b border-gray-500',
     tableHeading: 'py-3 w-[20%] font-medium text-white flex align-center',
-    tableBodyRow: 'bg-white border-b dark:bg-gray-800 dark:border-gray-700',
+    tableBodyRow: 'bg-white border-b bg-gray-800 border-gray-700',
     tableBodyDefinition: 'py-4 px-6',
     arrow: 'mt-2 text-2xl cursor-pointer',
     allArrows: 'flex justify-end',
