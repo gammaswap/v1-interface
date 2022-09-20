7661da6aaa38cbc197954e716d77c3a5318d8ca7
UsamaSaleem14 <usamasaleem0148@gmail.com>
2022-09-21 00:26:36 +0500

    Added dummy data for non subgraph users

--- src/hooks/usePoolsHandler.tsx
+++ src/hooks/usePoolsHandler.tsx
@@ -39,8 +39,37 @@ export const usePoolsHandler = () => {
       }
     }
 
-    if (poolData?.length === 0) {
-      fetchPoolsData()
+    if (process.env.NEXT_PUBLIC_SUBGRAPH_URL) {
+      if (poolData?.length === 0) {
+        fetchPoolsData()
+      }
+    } else {
+      setPoolData([
+        {
+          asset: 'Binance',
+          totalSupply: '20,223,182,626',
+          supplyApy: '895',
+          totalBorrowed: '20,960,370',
+          borrowApyVariable: '1683',
+          borrowApyStable: '394',
+        },
+        {
+          asset: 'Coinbase Exchange',
+          totalSupply: '2,815,007,914',
+          supplyApy: '945',
+          totalBorrowed: '1,756,438',
+          borrowApyVariable: '1274',
+          borrowApyStable: '5196',
+        },
+        {
+          asset: 'Gate.io',
+          totalSupply: '1,652,717,489',
+          supplyApy: '549',
+          totalBorrowed: '2,658,869',
+          borrowApyVariable: '2565',
+          borrowApyStable: '1448',
+        },
+      ])
     }
   }, [poolData])
 
