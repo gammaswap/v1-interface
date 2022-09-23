ad1b181871021f8253d8500dfe74d2c4333add59
Roberto Martinez <robertomiguel2001@gmail.com>
2022-09-22 23:45:01 -0400

    style(withdraw): improve slider component design

--- pages/positions/withdrawliquidity.tsx
+++ pages/positions/withdrawliquidity.tsx
@@ -20,10 +20,10 @@ const WithdrawLiquidity: NextPage = () => {
 
   const style = {
     wrapper: 'w-full h-full flex justify-center',
-    container: 'mt-20 bg-neutrals-800 w-[30rem] h-3/4 rounded-2xl p-4',
+    container: 'mt-20 bg-neutrals-800 w-[30rem] h-3/4 rounded-xl p-4',
     headerContainer: 'flex text-xxs',
-    backButton: 'w-6 h-6 mt-0.5',
-    formHeader: 'font-semibold text-lg text-neutrals-200 ml-4',
+    backButton: 'w-7 h-7 mt-0.5 cursor-pointer hover:bg-neutrals-700 p-1 rounded-full',
+    formHeader: 'font-semibold text-lg text-neutrals-100 ml-4',
     tabsContainer: 'flex rounded-lg w-1/4 p-0.5 bg-neutrals-900 bg-opacity-40 drop-shadow-md space-x-2 font-normal ml-auto',
     tab: 'text-neutrals-600 w-1/2 rounded-md hover:bg-neutrals-800 hover:text-neutrals-300',
     activeTab: 'outline outline-2 outline-offset-2 outline-accents-royalBlue/50 bg-accents-royalBlue w-1/2 rounded-md',
@@ -38,10 +38,11 @@ const WithdrawLiquidity: NextPage = () => {
     invalidatedButton:
       'w-full my-2 rounded-2xl py-3 px-5 text-xl font-semibold flex justify-center items-center text-gray-600 mt-8 border-2 border-gray-700',
     withdrawHeading: 'w-screen text-left',
-    sliderStyle: 'border-2 border-gray-800 shadow-lg mt-2 p-4 rounded-2xl',
-    sliderPercent: 'mb-5 text-6xl text-white rounded-xl',
-    percentageBox: 'flex justify-between mt-4',
-    percentages: 'bg-gray-800 text-white text-center py-3 px-6 cursor-pointer rounded-lg font-semibold',
+    sliderContainer: 'bg-neutrals-700 drop-shadow-md mt-5 p-4 rounded-lg',
+    sectionHeader: 'font-semibold mt-1 text-neutrals-400 w-full',
+    sliderPercent: 'text-6xl text-neutrals-100 mt-2',
+    percentageBoxContainer: 'flex justify-center space-x-8 mt-5',
+    percentageBox: 'bg-neutrals-800 text-neutrals-100 py-2 px-5 cursor-pointer drop-shadow-sm rounded-lg hover:text-primary-blue',
     dropdownArrow: 'w-5 h-5',
     downIcon: 'flex justify-center my-2',
     buttonDiv: 'flex justify-center',
@@ -49,7 +50,6 @@ const WithdrawLiquidity: NextPage = () => {
     eachAmount: 'flex justify-between p-2',
     totalPriceContainer: 'flex flex-col items-end text-gray-500 text-sm mt-2',
     unitTokenConversion: 'font-semibold',
-    sectionHeader: 'font-semibold text-gray-200 w-full',
   }
 
   return (
@@ -76,50 +76,54 @@ const WithdrawLiquidity: NextPage = () => {
                 </Tab>
               </Tab.List>
           </div>
-          <div className={style.formLabel}></div>
-          <div className={style.sliderStyle}>
+          <div className={style.sliderContainer}>
             <div className={style.sectionHeader}>Amount</div>
-            <p className={style.sliderPercent}>{sliderPercentage}%</p>
+            <h1 className={style.sliderPercent}>{sliderPercentage}%</h1>
             <Slider
               onChange={sliderPercentChange}
               value={sliderPercentage}
+              className="mt-5"
+              trackStyle={{
+                backgroundColor: '#549AF0',
+                height: '6px',
+              }}
+              railStyle={{
+                height: '6px',
+              }}
               handleStyle={{
-                borderColor: 'white',
+                borderColor: '#ECECED',
+                width: '16px',
+                height: '16px',
                 borderWidth: '2px',
-                backgroundColor: '#abe2fb',
+                backgroundColor: '#549AF0',
                 opacity: 1,
               }}
+              dotStyle={{
+                borderColor: 'red'
+              }}
             />
-            <div className={style.percentageBox}>
+            <div className={style.percentageBoxContainer}>
               <p
-                className={style.percentages}
-                onClick={() => {
-                  changeSliderPercentage(25)
-                }}
+                className={style.percentageBox}
+                onClick={() => changeSliderPercentage(25)}
               >
                 25%
               </p>
               <p
-                className={style.percentages}
-                onClick={() => {
-                  changeSliderPercentage(50)
-                }}
+                className={style.percentageBox}
+                onClick={() => changeSliderPercentage(50)}
               >
                 50%
               </p>
               <p
-                className={style.percentages}
-                onClick={() => {
-                  changeSliderPercentage(75)
-                }}
+                className={style.percentageBox}
+                onClick={() => changeSliderPercentage(75)}
               >
                 75%
               </p>
               <p
-                className={style.percentages}
-                onClick={() => {
-                  changeSliderPercentage(100)
-                }}
+                className={style.percentageBox}
+                onClick={() => changeSliderPercentage(100)}
               >
                 Max
               </p>
