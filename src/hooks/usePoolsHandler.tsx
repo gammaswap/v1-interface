bda239b6b22704e13aca15362a16ba16b66fcf60
Roberto Martinez <robertomiguel2001@gmail.com>
2022-10-18 02:09:35 -0400

    refactor(pool-queries): improve render logic for latest pool data

--- src/hooks/usePoolsHandler.tsx
+++ src/hooks/usePoolsHandler.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { notifyError } from './useNotification'
 import { ExecutionResult } from 'graphql'
 import { PoolsDocument, Pool, PoolsQuery, LatestPoolDataDocument, execute, PoolData } from '../../.graphclient'
@@ -7,44 +7,47 @@ export const usePoolsHandler = () => {
   const [pools, setPools] = useState<ExecutionResult<PoolsQuery>>()
   const [latestPoolsData, setLatestPoolsData]  = useState<Array<PoolData>>([])
   
-  useEffect(() => {
-    const fetchPoolsData = async () => {
-      const res = await execute(PoolsDocument, {})
-      if (res?.data) {
-        setPools(res.data.pools)
-      } else {
-        console.log("NO RESPONSE")
+  // fetches all pool entities
+  const fetchPoolsData = useCallback(async () => {
+    const res = await execute(PoolsDocument, {})
+    if (res?.data) {
+      setPools(res.data.pools)
+    } else {
+      console.log("NO RESPONSE")
+    }
+  }, [])
+  
+  // fetches pool's latest data by pool address
+  const fetchLatestPoolData = async (address: string): Promise<PoolData | number> => {
+    const res = await execute(LatestPoolDataDocument, { address })
+    if (res?.data.poolDatas[0]) {
+      return res.data.poolDatas[0]
+    }
+  
+    return 0
+  }
+  
+  // iterates through all pool addresses and fetches latest pool data for it
+  const fetchLatestPoolsData = useCallback(async (pools: Array<Pool>) => {
+    const newPoolsData: Array<PoolData> = []
+
+    for (const pool of pools) {
+      const latestPoolData: PoolData | number = await fetchLatestPoolData(pool.address)
+      if (latestPoolData != 0) {
+        newPoolsData.push(latestPoolData as PoolData)
       }
     }
     
-    fetchPoolsData()
+    setLatestPoolsData(newPoolsData)
   }, [])
   
   useEffect(() => {
-    const fetchLatestPoolData = async (address: string): Promise<PoolData | number> => {
-      const res = await execute(LatestPoolDataDocument, { address })
-      if (res?.data.poolDatas[0]) {
-        return res.data.poolDatas[0]
-      }
+    fetchPoolsData()
+  }, [fetchPoolsData])
 
-      return 0
-    }
-
-    const fetchLatestPoolsData = async (pools: Array<Pool>) => {
-      // query latest pool data for each addr
-      // push to latestPoolsData
-      pools?.forEach(async pool => {
-        const latestPoolData: PoolData | number = await fetchLatestPoolData(pool.address)
-        if (latestPoolData != 0) {
-          const newPoolsData = [...latestPoolsData]
-          newPoolsData.push(latestPoolData as PoolData)
-          setLatestPoolsData(newPoolsData)
-        }
-      })
-    }
-    
-    fetchLatestPoolsData(pools as Array<Pool>)
-  }, [])
-
+  useEffect(() => {
+    if (pools) fetchLatestPoolsData(pools as Array<Pool>)
+  }, [pools])
+  
   return { pools, latestPoolsData }
 }
